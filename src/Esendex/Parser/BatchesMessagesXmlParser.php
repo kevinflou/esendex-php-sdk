e0ee5deebd09917fa359d4f1b6e69eb4ea41e7c3
Kevin Flou <kevin@seeyoucloud.com>
2020-02-17 14:49:25 +0100

    MessagesBatcheS

--- src/Esendex/Parser/BatchesMessagesXmlParser.php
+++ src/Esendex/Parser/BatchesMessagesXmlParser.php
@@ -2,7 +2,7 @@
 /**
  * Copyright (c) 2013, Esendex Ltd.
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above copyright
@@ -13,7 +13,7 @@
  *     * Neither the name of Esendex nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -38,7 +38,7 @@ use Esendex\Model\BatchesMessage;
 
 class BatchesMessagesXmlParser
 {
-  
+
     public function parse($xml)
     {
         $header = simplexml_load_string($xml);
@@ -47,11 +47,11 @@ class BatchesMessagesXmlParser
 
     public function parseHeader($header)
     {
-
+        //dd($header);
         $result = array();
-        foreach ($header->messageheader as $message) {
+        foreach ($header->messagebatch as $message) {
+            //dd($message);
             $item = new BatchesMessage();
-            $item->phonenumber($message->to[0]->phonenumber);
             $item->status($message->status);
             $item->id($message['id']);
             $result[] = $item;
